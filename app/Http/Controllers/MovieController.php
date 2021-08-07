616d6805792a178aaf6a086a0c05288d9fe6a98d
yutaishikawa <iy.24.bump.09@gmail.com>
2021-08-07 17:07:33 +0900

    [add] #22 add review in twitter

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -3,16 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Vender\CallTwitterApi;
 
 use App\Review;
 
 class MovieController extends Controller
 {
-    public function show($movie_id) {
+    public function show($movie_id, $movie_name) {
+        //レビュー検索
         $reviews = Review::where('movie_id',$movie_id)->get();
+        //Tweet検索
+        $twitter = new CallTwitterApi();
+        $tweet_results = $twitter->search('#'.$movie_name);
+        //埋め込み式に
+        $formed_tweet_results = array();
+        foreach($tweet_results as $tweet_result) {
+          $formed_tweet_results[] = array($twitter->statusesOembed($tweet_result->id));
+        }
+
         return view('movie.show')->with([
             'movie_id' => $movie_id,
             'reviews' => $reviews,
+            'tweets' => $formed_tweet_results
         ]);
     }
 }
--- resources/views/movie/show.blade.php
+++ resources/views/movie/show.blade.php
@@ -6,24 +6,43 @@
         <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
         <title>映画クチコミサイト MusicTheater</title>
+        <style>
+          [v-cloak] {
+              display: none;
+          }
+        </style>
     </head>
     <body>
-        <div id="app">
+        <div id="app" v-cloak>
             <div>
                 <a :href="data.homepage"><img :src="'https://image.tmdb.org/t/p/w185' + data.poster_path"></a> 
             </div>
             <div>タイトル:@{{ data.title }}</div>
-            <div v-if="data.overview">あらすじ:@{{ data.overview }}</div>
-            <div>公開日:@{{ data.release_date }}</div>
-            <br>
-            <div>
-            <p>口コミ</p>
-            @foreach($reviews as $review)
-            <div>
-            {{ $review->title }} / {{ $review->user->name }} / {{ $review->created_at }}
-            <p> {{ $review->body }} </p> 
+              <div v-if="data.overview">あらすじ:@{{ data.overview }}</div>
+              <div>公開日:@{{ data.release_date }}</div>
+              <br>
             </div>
-            @endforeach
+            <div>
+              @if($tweets)
+                <h3>Twitter上の口コミ</h3>
+                @foreach($tweets as $tweet)
+                  {!! $tweet[0] !!}<br>
+                @endforeach
+              @endif
+            </div>
+            <div>
+              <h3>口コミ</h3>
+              @if($tweets)
+                @foreach($reviews as $review)
+                <div>
+                    {{ $review->title }} / {{ $review->user->name }} / {{ $review->created_at }}
+                    <p> {{ $review->body }} </p> 
+                </div>
+                @endforeach
+              @else
+                口コミはまだ投稿されていません。
+              @endif
+            </div>
             <p>--------投稿フォーム---------</p>
             <form method="post" action="/review/{{$movie_id}}/post">
             @csrf
@@ -41,17 +60,17 @@
                 data() {
                     return {
                         data: '',
-                        app_key: '{{ env('TMDB_APP_KEY') }}'
-                    };
+                        tmdb_app_key: '{{ env('TMDB_APP_KEY') }}',
+                    }
                 },
                 methods: {},
                 mounted() {
                     var movieId =  window.location.href.split('/')[4]
-                    const url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${this.app_key}&language=ja-JA`
-                    console.log(url)
+                    const url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${this.tmdb_app_key}&language=ja-JA`
                     axios.get(url)
                         .then(response => {
                         this.data = response.data;
+                        this.getTweet();
                         }).catch(err => {
                         console.log('err:', err);
                         return
