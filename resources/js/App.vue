fd06981727e2ce553a56bc1ff6fb0b965216489e
yuta316 <iy.24.bump.09@gmail.com>
2021-08-27 00:54:31 +0900

    [add] #29 Introducing vue-router

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    a
+    <router-view></router-view>
   </div>
 </template>
 
--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,10 @@ window.Vue = require('vue');
 
 import App from './App.vue';
 
+import router from './router'
+
 const app = new Vue({
     el: '#app',
     render: (h) => h(App), //id="app"の部分にAppコンポーネントを差し込む
+    router,
 });
