c84ef830602b759c0e9c3f3acab3a6bc02e16f10
yuta316 <iy.24.bump.09@gmail.com>
2021-09-22 02:51:54 +0900

    [add] #45 add login form

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,16 +1,21 @@
 window.Vue = require('vue');
 
-import Router from 'vue-router'
+import Router from 'vue-router';
 Vue.use(Router)
 
 // トップ画面を構成するLayout
-import Layout from './layout/Layout'
+import Layout from './layout/Layout';
+import LoginForm from './auth/login';
 
 export default new Router({
     mode: 'history',
     routes: [
         {
-            path: '',
+            path: '/login',
+            component: LoginForm
+        },
+        {
+            path: '/',
             component: Layout,
             children: [
                 {
