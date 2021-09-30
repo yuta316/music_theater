663c28f77a8032d957e2caadc2fa268ab555b618
yuta316 <iy.24.bump.09@gmail.com>
2021-10-01 04:53:01 +0900

    [add] #25 line login

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,14 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
-Route::post('/login', 'Auth\LoginController@login');
+Route::prefix('login')->group(function () {
+    Route::get('', 'Auth\LoginController@showLoginForm')->name('login');
+    Route::post('', 'Auth\LoginController@login');
 
-
-Route::get('/login/{social}', 'Auth\LoginController@redirectToProvider');
-Route::get('/login/{social}/callback', 'Auth\LoginController@handleProviderCallback');
+    Route::get('line', 'LineLoginController@index');
+    Route::get('{social}', 'Auth\LoginController@redirectToProvider');
+    Route::get('line/callback', 'LineLoginController@handleProviderCallback');
+    Route::get('{social}/callback', 'Auth\LoginController@handleProviderCallback');
+});
 
 Route::middleware('auth')->group(function (){
     Route::post('/loggedout', function(){
